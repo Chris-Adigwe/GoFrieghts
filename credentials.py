3181d0d0529d5a5fcdc52ad1b725063df5ac8bc5
Chris-Adigwe <chris.adigwe@yahoo.com>
2023-05-02 09:25:42 +0100

    added a query function question

--- credentials.py
+++ credentials.py
@@ -6,6 +6,8 @@ from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 import pyodbc
 import requests, zipfile
+import pandas as pd
+import psycopg2
 
 def get_database_conn():
     dotenv.load_dotenv(r"C:\Users\NGSL0161\Desktop\Data Engineering class\projects\environmental variables\.env")
@@ -18,6 +20,23 @@ def get_database_conn():
 
 def get_accessbase_conn():
     return pyodbc.connect( r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=data/WPI.mdb')
+
+
+def question(query,question):
+    db_user_name, db_password, db_name, port, host = get_database_conn()
+    
+    conn = psycopg2.connect(f'dbname={db_name} user={db_user_name} password={db_password} host={host} port={port}')
+    
+    # Execute the query and fetch the results
+    results = pd.read_sql_query(query, conn)
+    results.to_csv(f'data/question_{question}.csv', index=False)
+    
+    # Close the database connection
+    conn.close()
+
+
+
+
 
 
 
