ae9f76cea470b573e21c90c467065df954dd465c
Chris-Adigwe <chris.adigwe@yahoo.com>
2023-06-08 19:14:48 +0100

    updated the main function

--- main.py
+++ main.py
@@ -9,22 +9,17 @@ from sqlalchemy import create_engine
 from sqlalchemy import text
 import dotenv, os
 from dotenv import dotenv_values
-from credentials import get_database_conn, get_accessbase_conn
+from utils import get_database_conn, get_accessbase_conn, question
 from etl import transform_file, load_data, extract_from_google
 
 
 def main():
-    conn = get_accessbase_conn()
-    db_user_name, db_password, db_name, port, host = get_database_conn()
-    # Determin if there is a scrapped data. If this is True, the data is transformed and loaded.
-    # If data does not exist, the data is extracted, transformed and loaded.
-    if os.path.exists('GoFrieghts/data/WPI.mdb'):
-        transform_file()
-        load_data(db_user_name, db_password, db_name, port, host)
-    else:
-        extract_from_google()
-        transform_file()
-        load_data(db_user_name, db_password, db_name, port, host)
+    
+    conn = get_database_conn()
+    extract_from_google()
+    transform_file()
+    load_data()
+    question()
 
 
 # Execute the script by invoking the main method
